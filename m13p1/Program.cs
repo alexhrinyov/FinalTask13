480112d68097f072d830ebbce7673c26392d2e70
alexhrinyov <hrinyou29@gmail.com>
2022-12-15 21:52:10 +0300

    Add project files.

---
+++ m13p1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.Diagnostics;
+
+namespace m13p1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Сравнение производительности списка и связного списка
+
+            List<char> list = new List<char>();
+            LinkedList<char> linkedList = new LinkedList<char>();
+            string workingDirectory = Environment.CurrentDirectory;
+            string projectDirectory = Directory.GetParent(workingDirectory).Parent.Parent.FullName;
+            // читаем весь файл с рабочего стола в строку текста
+            string text = File.ReadAllText(Path.Combine(projectDirectory, "Text1.txt"));
+            foreach (var item in text)
+            {
+                list.Add(item);
+                linkedList.AddFirst(item);
+            }
+            // Проверяем обычный список
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+            for (int i = 0; i < 4000; i++)
+            {
+                list.Insert(500, 'M');
+            }
+            stopWatch.Stop();
+            // Get the elapsed time as a TimeSpan value.
+            TimeSpan ts = stopWatch.Elapsed;
+            // Результат
+            Console.WriteLine($"list: {ts.TotalMilliseconds.ToString()}");
+            // Проверяем связный список
+            LinkedListNode<char> node = linkedList.First;
+            Stopwatch stopWatch2 = new Stopwatch();
+            stopWatch2.Start();
+            for (int i = 0; i < 4000; i++)
+            {
+                linkedList.AddAfter(node, 'p');
+            }
+            stopWatch2.Stop();
+            TimeSpan ts2 = stopWatch2.Elapsed;
+            // Результат
+            Console.WriteLine($"LinkedList: {ts2.TotalMilliseconds}");
+
+        }
+    }
+}
---
+++ m13p2/Program.cs
@@ -0,0 +1,44 @@
+﻿
+using static System.Net.Mime.MediaTypeNames;
+
+namespace m13p2
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            List<char> list = new List<char>();
+            LinkedList<char> linkedList = new LinkedList<char>();
+            string workingDirectory = Environment.CurrentDirectory;
+            string projectDirectory = Directory.GetParent(workingDirectory).Parent.Parent.FullName;
+            // читаем весь файл с рабочего стола в строку текста
+            string text = File.ReadAllText(Path.Combine(projectDirectory, "Text1.txt"));
+            string cleaned = text.Replace("\n", " ");
+            var noPunctuationText = new string(cleaned.Where(c => !char.IsPunctuation(c)).ToArray());
+            //Разобьем на массив слов
+            string[] words = noPunctuationText.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            SortedDictionary<string, int> wordCountPair = new SortedDictionary<string, int>();
+            foreach (var word in words)
+            {
+                if (wordCountPair.ContainsKey(word))
+                    wordCountPair[word]++;
+                else
+                    wordCountPair.Add(word, 1);
+            }
+            int maxValue = wordCountPair.Values.Max();
+            
+            var mRep = from wc in wordCountPair
+                       where wc.Value == maxValue
+                       select wc.Key;
+            foreach (var item in mRep)
+            {
+                Console.WriteLine($"Слово -{item}- встречается {maxValue} раз в этом тексте.");
+            }
+            
+            
+
+
+        }
+    }
+}
